b41aecc4eb60af57cfb966d78415981389f605a5
s23sean321 <141469984+s23sean321@users.noreply.github.com>
2023-09-14 17:53:01 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
 from flask import Flask,request,abort
-from events.service import *
+#from events.service import *
 from line_bot_api import *
 from events.basic import *
-from events.admin import *
-from extensions import db, migrate
-from models.user import User    
+#from events.admin import *
+#from extensions import db, migrate
+#from models.user import User    
 
 import os
 
 app = Flask(__name__)
 
-app.config.from_object(os.environ.get('APP_SETTINGS','config.DevConfig'))
-app.config['SQLALCHEMY_DATABASE_URI'] ='postgresql://s23sean321:pdKVrYpBstM7Eu7NkMyCBhKntRaPlgwH@dpg-cjiu1or37aks73cr6cd0-a.singapore-postgres.render.com/aigogogosql'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] =False
-db.app=app
-db.init_app(app)
-migrate.init_app(app,db)
+#app.config.from_object(os.environ.get('APP_SETTINGS','config.DevConfig'))
+#app.config['SQLALCHEMY_DATABASE_URI'] ='postgresql://s23sean321:pdKVrYpBstM7Eu7NkMyCBhKntRaPlgwH@dpg-cjiu1or37aks73cr6cd0-a.singapore-postgres.render.com/aigogogosql'
+#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] =False
+#db.app=app
+#db.init_app(app)
+#migrate.init_app(app,db)
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -45,21 +45,21 @@ def callback():
 def handle_message(event):
 
     message_text = str(event.message.text).lower()
-    user = User.query.filter(User.line_id == event.source.user_id).first()
+#    user = User.query.filter(User.line_id == event.source.user_id).first()
 
-    if not user:
-        profile = line_bot_api.get_profile(event.source.user_id)
-        print(profile.display_name)
-        print(profile.user_id)
-        print(profile.picture_url)
+#    if not user:
+#        profile = line_bot_api.get_profile(event.source.user_id)
+#        print(profile.display_name)
+#        print(profile.user_id)
+#        print(profile.picture_url)
 
-        user = User(profile.user_id,profile.display_name,profile.picture_url)
-        db.session.add(user)
-        db.session.commit()
+#        user = User(profile.user_id,profile.display_name,profile.picture_url)
+#        db.session.add(user)
+#        db.session.commit()
 
-    print(user.id)
-    print(user.line_id)
-    print(user.display_name)
+#    print(user.id)
+#    print(user.line_id)
+#    print(user.display_name)
 
 
     if message_text == '@關於我們':
@@ -68,33 +68,33 @@ def handle_message(event):
     elif message_text == '@營業據點':
         location_event(event)
 
-    elif message_text == '@預約服務':
-        service_category_event(event)
+#    elif message_text == '@預約服務':
+#        service_category_event(event)
 
-    elif message_text.startswith('*'):
-        if event.source.user_id not in ['U23237fa985e2d6cdc28a38cea1d592e4']:
-            return
-        if message_text in ['*data','*d']:
-            list_reservation_event(event)
+#    elif message_text.startswith('*'):
+#        if event.source.user_id not in ['U23237fa985e2d6cdc28a38cea1d592e4']:
+#            return
+#        if message_text in ['*data','*d']:
+#            list_reservation_event(event)
         
 
 
 
-@handler.add(PostbackEvent)
-def handle_postback(event):
-    data = dict(parse_qsl(event.postback.data))
-    if data.get('action')=='service':
-        service_event(event)
-    elif data.get('action') =='select_date':
-        service_select_date_event(event)
-    elif data.get('action') == 'select_time':
-        service_select_time_event(event)
-    elif data.get('action') == 'confirm':
-        service_confirm_event(event)
-    elif data.get('action') == 'confirmed':
-        service_confirmed_event(event)
-    elif data.get('action') == 'cancel':
-        service_cancel_event(event)
+#@handler.add(PostbackEvent)
+#def handle_postback(event):
+#    data = dict(parse_qsl(event.postback.data))
+#    if data.get('action')=='service':
+#        service_event(event)
+#    elif data.get('action') =='select_date':
+#        service_select_date_event(event)
+#    elif data.get('action') == 'select_time':
+#        service_select_time_event(event)
+#    elif data.get('action') == 'confirm':
+#        service_confirm_event(event)
+#    elif data.get('action') == 'confirmed':
+#        service_confirmed_event(event)
+#    elif data.get('action') == 'cancel':
+#        service_cancel_event(event)
 
 
 
